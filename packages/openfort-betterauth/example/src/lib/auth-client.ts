2f3111fc5e91a4710c682e292bbe0ceab227e212
Jaume Alavedra <jamalavedra@gmail.com>
2025-11-11 17:53:26 +0100

    chore: required bearer plugin

--- packages/openfort-betterauth/example/src/lib/auth-client.ts
+++ packages/openfort-betterauth/example/src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 import { openfortClient } from "@openfort/better-auth";
+import { bearer } from "better-auth/plugins";
 
 export const authClient = createAuthClient({
 	baseURL: "http://localhost:3000", // the base url of your auth server
-	plugins: [openfortClient()],
+	plugins: [openfortClient(), bearer()],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
